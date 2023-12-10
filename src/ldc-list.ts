f98a7f8e7480fdd405617c598f5291aca73ec7bc
Lee Cooper <cooperlee372@gmail.com>
2023-12-10 17:46:24 -0500

    add basic lit element component, change build output dir, remove build output files from root dir

---
+++ src/ldc-list.ts
@@ -0,0 +1,12 @@
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators.js";
+
+
+@customElement('ldc-list')
+export class LdcList extends LitElement {
+
+
+    override render() {
+         return html `hello`;
+    }
+}
